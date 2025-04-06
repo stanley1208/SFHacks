68450388d59a39e5e212f76603dbd4a9584afa61
MEENOYY <jd052321@naver.com>
2025-04-06 00:21:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -402,6 +402,8 @@ def answer_question(email):
     return redirect(url_for('get_transactions', email=email))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
